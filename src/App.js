44b767cd2bddcd23d4586ca85eac5100366781a5
anchal66 <anchalavinash66@gmail.com>
2022-01-27 18:21:19 +0530

    initial project for next lesson

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
-import Todo from "./components/Todo";
-
 function App() {
   return (
     <div>
-      <h1>My Todo</h1>
-      <Todo title="Learn React"/>
-      <Todo title="Master React"/>
     </div>
   );
 }
