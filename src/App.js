8c26e22de1e4f1d5dd6ac13810d904c06280c5a2
anchal66 <anchalavinash66@gmail.com>
2022-01-27 19:17:26 +0530

    Use props.children as Card and Layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Route, Switch } from "react-router-dom";
-import MainNavigation from "./components/layout/main-navigation/MainNavigation";
+import Layout from "./components/layout/Layout/Layout";
 import AllMeetupsPage from "./pages/AllMeetups";
 import FavoritesPage from "./pages/Favorites";
 import NewMeetupsPage from "./pages/NewMeetup";
@@ -7,18 +7,19 @@ import NewMeetupsPage from "./pages/NewMeetup";
 function App() {
   return (
     <div>
-      <MainNavigation />
-      <Switch>
-        <Route exact path="/">
-          <AllMeetupsPage />
-        </Route>
-        <Route path="/new-meetup">
-          <NewMeetupsPage />
-        </Route>
-        <Route path="/favorites">
-          <FavoritesPage />
-        </Route>
-      </Switch>
+      <Layout>
+        <Switch>
+          <Route exact path="/">
+            <AllMeetupsPage />
+          </Route>
+          <Route path="/new-meetup">
+            <NewMeetupsPage />
+          </Route>
+          <Route path="/favorites">
+            <FavoritesPage />
+          </Route>
+        </Switch>
+      </Layout>
     </div>
   );
 }
